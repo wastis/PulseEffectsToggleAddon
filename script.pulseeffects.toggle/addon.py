85d6af0f7747adfc1c57ac7215111fd122c07b22
wastis <75071916+wastis@users.noreply.github.com>
2021-02-15 22:08:30 +0100

    Add files via upload

--- script.pulseeffects.toggle/addon.py
+++ script.pulseeffects.toggle/addon.py
@@ -13,17 +13,20 @@ def run(cmd):
 	return output
 	
 
-result = run("/usr/bin/flatpak run com.github.wwmm.pulseeffects -p")
+result = run("export LC_ALL=C;/usr/bin/flatpak run com.github.wwmm.pulseeffects -p")
 
-if(result[0].find("com.github.wwmm.pulseeffects") != -1): 
+oline = ""
+for line in result:
+    if line[:15] == "Output Presets:": oline = line;
 
+if(oline == ""): 
 	xbmcgui.Dialog().textviewer("PulseEffects", "Error with PulseEffects, please refer Kodi log.", False) 
 	xbmc.log('\n'.join(result) , xbmc.LOGERROR) 
 	
 	
 else:
 
-	profile_list = result[0].split(':')[1].strip().split(',')[:-1]
+	profile_list = oline.split(':')[1].strip().split(',')[:-1]
 	
 	if(len(profile_list)<1):
 	
